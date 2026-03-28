3ed78f724ec00d0537b364b45e9cba89db01b3dc
One <wungamertv@gmail.com>
2026-03-29 01:59:47 +0700

    merge

--- project/frontend/frontend-admin/vite.config.js
+++ project/frontend/frontend-admin/vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    allowedHosts: 'all',
+    cors: true
+  }
 })
